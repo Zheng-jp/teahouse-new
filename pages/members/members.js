29ebde5eadfddda40096d97c29066f6fe7e6a466
zoume1 <595649828@qq.com>
2018-11-16 18:20:24 +0800

    la

--- pages/members/members.js
+++ pages/members/members.js
@@ -26,6 +26,47 @@ Page({
     ]
 
   },
+  pay: function () {
+    var ordercode = this.data.txtOrderCode;
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          wx.request({
+            url: 'https://www.yourdomain.com/pay',
+            data: {
+              code: res.code,//要去换取openid的登录凭证
+              ordercode: ordercode
+            },
+            method: 'GET',
+            success: function (res) {
+              console.log(res.data)
+              wx.requestPayment({
+                timeStamp: res.data.timeStamp,
+                nonceStr: res.data.nonceStr,
+                package: res.data.package,
+                signType: 'MD5',
+                paySign: res.data.paySign,
+                success: function (res) {
+                  // success
+                  console.log(res);
+                },
+                fail: function (res) {
+                  // fail
+                  console.log(res);
+                },
+                complete: function (res) {
+                  // complete
+                  console.log(res);
+                }
+              })
+            }
+          })
+        } else {
+          console.log('获取用户登录态失败！' + res.errMsg)
+        }
+      }
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
