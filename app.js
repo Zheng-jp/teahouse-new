e8c74508ff039dc5592451bda18db9f02887b99a
zoume1 <595649828@qq.com>
2018-12-26 14:30:39 +0800

    pull

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ App({
     gmemberid:null,
 
     url:'https://teahouse.siring.com.cn',
-    // tiltes: 'https://teahouse.siring.com.cn/',
-    // img_url: 'https://teahouse.siring.com.cn/uploads/'
-    tiltes:'http://localhost/teahouse/public/',
-    img_url:' http://localhost/teahouse/public/uploads/'
+    tiltes: 'https://teahouse.siring.com.cn/',
+    img_url: 'https://teahouse.siring.com.cn/uploads/',
+    // url:'http://localhost/teahouse.siring.com.cn',
+    // tiltes:'http://localhost/teahouse/public/',
+    // img_url:' http://localhost/teahouse/public/uploads/'
   }
 })
