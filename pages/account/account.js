006fb7145e7170017d6b51e20e03dc4a1adca6ba
zoume1 <595649828@qq.com>
2019-01-11 18:50:42 +0800

    pull

--- pages/account/account.js
+++ pages/account/account.js
@@ -5,27 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
+    ispassword:false,
 
   },
   go: function (event) {
 
     var that = this;
     var item = event.currentTarget.dataset.src;
-  
-    wx.navigateTo({
-      url: item,
-      success: function (res) {
-      
-      },
-      fail: function () {
-       
-      },
-      complete: function () {
-      
+    if(item=='../recharge/recharge'){
+      if(!that.data.ispassword){
+        wx.showModal({
+          title:'请设置支付密码',
+          content: '您还没有资金账号，为了保证您的资金安全，请先设置资金账号支付密码。设置后才可以进行充值、余额消费等操作',
+          confirmText:'马上设置',
+          confirmColor:'#3399FF',
+          cancelColor:'#bbb',
+          success: function(res) {
+            if (res.confirm) {
+              wx.navigateTo({
+                url: '../password/password',
+                success: function (res) {
+                
+                },
+                fail: function () {
+                 
+                },
+                complete: function () {
+                
+                }
+          
+          
+              })
+            } else if (res.cancel) {
+            console.log('用户点击取消')
+            }
+            }
+        })
       }
-
-
-    })
+      else{
+        wx.navigateTo({
+          url: item,
+          success: function (res) {
+          
+          },
+          fail: function () {
+           
+          },
+          complete: function () {
+          
+          }
+    
+    
+        })
+      }
+     
+    }
+    else{
+      wx.navigateTo({
+        url: item,
+        success: function (res) {
+        
+        },
+        fail: function () {
+         
+        },
+        complete: function () {
+        
+        }
+  
+  
+      })
+    }
+ 
   },
 
   /**
--- pages/password/password.js
+++ pages/password/password.js
@@ -1,11 +1,78 @@
-// pages/password/password.js
+// pages/add_address/add_address.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    region: ['省', '市', '区'],
+    customItem: "全部"
 
+  },
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
+  },
+  formSubmit: function (e) {
+    var that=this;
+   
+    wx.request({
+      url: app.globalData.tiltes + 'member_address_adds',
+      data: {
+        harvester: e.detail.value.harvester,
+        harvester_phone_num : e.detail.value.harvester_phone_num,
+        address_name: that.data.region,
+        harvester_real_address : e.detail.value.harvester_real_address,
+        status : 1,
+        open_id: app.globalData.gmemberid,
+      },
+      method: "post",
+      // header: {
+      //   "Content-Type": "json" // 默认值
+
+      // },
+      success: function (res) {
+        if(res.data.status==1){
+          wx.showToast({
+            title:res.data.info,
+            icon:'none',
+          });
+          
+        setTimeout(function () {
+          wx.navigateTo({
+            url: '../select_address/select_address',
+            success: function (res) {
+              // success
+              console.log("nihao////跳转成功")
+            },
+            fail: function () {
+              // fail
+              console.log("nihao////跳转失败")
+            },
+            complete: function () {
+              // complete
+              console.log("nihao////跳转行为结束，未知成功失败")
+            }
+
+          })
+        }, 2000)
+         
+        }
+       
+        console.log(res);
+     
+      },
+      fail: function () {
+
+      },
+      complete: function () {
+        wx.hideLoading()
+      }
+
+    });
   },
 
   /**
