85a2ae187f287c89427c8b8667c8e60eb0c88e82
chenxu11oopp <you@example.com>
2019-01-14 11:05:36 +0800

    chenxu

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
-
-//app.js
 App({
-
-  data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    winMask_if: true,
-
-  },
-  globalData: {
-    
-    userInfo: null,
-    gmemberid:null,
-
-    url:'https://teahouse.siring.com.cn',
-    tiltes: 'https://teahouse.siring.com.cn/',
-    img_url: 'https://teahouse.siring.com.cn/uploads/',
-    // url:'http://localhost/teahouse.siring.com.cn',
-    // tiltes:'http://localhost/teahouse/public/',
-    // img_url:' http://localhost/teahouse/public/uploads/'
+  onLaunch: function () {
 
   }
-
 })
