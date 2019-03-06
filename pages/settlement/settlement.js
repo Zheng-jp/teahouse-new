edf16943dc73ab5febad71eb6313c22c5dd7610a
zoume1 <595649828@qq.com>
2019-03-06 14:08:13 +0800

    pull

--- pages/settlement/settlement.js
+++ pages/settlement/settlement.js
@@ -37,13 +37,16 @@ Page({
     // 茶叶类型
     order_type:1,
     // 优惠劵显示
-    coupon_show:null,
+    coupon_show:null,//显示有无优惠劵
     showPayPwdInput: false,  //是否展示密码输入层
     pwdVal: '',  //输入的密码
     payFocus: true, //文本框焦点
     order_number:null,//下单订单号
-    
-    
+    coupon_order:[],//优惠劵弹窗显示
+    coupon_mark:false,
+    coupon_content:"有可适用优惠券",
+    money:0,
+    coupon_id:0,//使用优惠劵id
   },
   // 弹窗
   powerDrawer: function (e) {
@@ -262,6 +265,7 @@ Page({
           address_id:that.data.address_id,
           order_amount: that.data.all_money,
           order_type:that.data.order_type,
+          coupon_id:that.data.coupon_id
       },
       method: "post",
 
@@ -382,6 +386,7 @@ Page({
       address_id:that.data.address_id,
       order_amount: that.data.all_money,
       order_type:that.data.order_type,
+      coupon_id:that.data.coupon_id
 
       },
       method: "post",
@@ -523,37 +528,52 @@ Page({
   },
   go_coupon:function(){
     var that=this;
-    let userStr=JSON.stringify(that.data.user[1].good_id);
-    let userStr1=JSON.stringify(that.data.user[3].num);
-    var num=new Array();
-    num=[that.data.num];
-    let userStr3=JSON.stringify(num);
-    // 直接结算
-    if(that.data.from_buy){
-      wx.navigateTo({
-        url: '../user_coupon/user_coupon?title=' + userStr+'&num='+userStr3,
-        success: function (res) {
-        },
-        fail: function () {
-        },
-        complete: function () {
-        }
-  
-      })
-    }
-    else{
-      wx.navigateTo({
-        url: '../user_coupon/user_coupon?title=' + userStr+'&num='+userStr1,
-        success: function (res) {
-        },
-        fail: function () {
-        },
-        complete: function () {
-        }
-  
-      })
-    }
+    that.setData({
+      coupon_mark:true,
+    })
    
+  },
+  checkboxChange: function (e) {
+    var that=this;
+    console.log(e.detail.value);
+    wx.request({
+      url: app.globalData.tiltes + 'coupon_minute',
+      data: {
+        coupon_id:e.detail.value[0],
+      },
+      method: "post",
+      // header: {
+      //   "Content-Type": "json" // 默认值
+
+      // },
+      success: function (res) {
+         that.setData({
+          coupon_content:"-"+ res.data.data.money,
+          money:res.data.data.money
+        });
+        that.calculate_money();
+      },
+      fail: function () {
+
+      },
+      complete: function () {
+      }
+
+    });
+    that.setData({
+      coupon_mark:false,
+      coupon_id:e.detail.value,
+    })
+  },
+  no_use: function (e) {
+    var that=this;
+    that.setData({
+      coupon_mark:false,
+      coupon_content:"有可适用优惠券",
+      coupon_id:0,
+      money:0,
+    })
+    that.calculate_money();
   },
      // 计算钱
      calculate_money:function(){
@@ -564,7 +584,7 @@ Page({
      }
      
      that.setData({
-       all_money: all_moneys,
+       all_money: all_moneys-that.data.money,
      });
    },
   
@@ -775,7 +795,8 @@ Page({
           // },
           success: function (res) {
               that.setData({
-                coupon_show:res.data.status
+                coupon_show:res.data.status,
+                coupon_order:res.data.data,
               });
             
             
@@ -862,28 +883,7 @@ Page({
       }
 
     });
-    wx.request({
-      url: app.globalData.tiltes + 'coupon_minute',
-      data: {
-        coupon_id:coupon_id,
-      },
-      method: "post",
-      // header: {
-      //   "Content-Type": "json" // 默认值
-
-      // },
-      success: function (res) {
-         that.setData({
-          coupon_money: res.data.data.money,
-        });
-      },
-      fail: function () {
-
-      },
-      complete: function () {
-      }
-
-    });
+    
   },
 
   /**
