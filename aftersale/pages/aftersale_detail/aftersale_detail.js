70767992e24f4698d09ddac5671eadbfc46f9fdd
youdianku <571851168@qq.com>
2019-03-12 15:44:46 +0800

    push

--- aftersale/pages/aftersale_detail/aftersale_detail.js
+++ aftersale/pages/aftersale_detail/aftersale_detail.js
@@ -11,6 +11,16 @@ function countDown(endTime, _this){
     countDownTime: Day + '天' + Hour + '时' + Minutes + '分' + Second + '秒'
   })
 }
+// 时间戳转换
+function timeTrans(date){
+  var date = new Date(1552356735*1000);
+  var Y = date.getFullYear();
+  var M = date.getMonth() + 1;
+  var D = date.getDate();
+  var H = date.getHours();
+  var MM = date.getMinutes();
+  return Y + '-' + M + '-' + D + ' ' + H + ':' + MM;
+}
 
 function addZero(num){
   return num > 10 ? num : '0' + num;
@@ -23,11 +33,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    endTime: 1553164429000,
     countDownTime: '',
     id: null,
     status: null,
     dataObj: {},
+    operationTime: '',
+    whoHandle: null,
+  },
+
+  revoke: function(){
+    var _this = this;
+    // 撤销申请
+    wx.showModal({
+      title: '提示',
+      content: '是否撤销申请？',
+      success(res){
+        if(res.confirm) {
+          wx.request({
+            url: app.globalData.tiltes + 'cancellation_of_application',
+            method: 'POST',
+            data: {
+              after_sale_id: _this.data.id
+            },
+            success: function(res){
+              console.log('撤销成功', res);
+            },
+            fail: function(){
+              console.log('撤销失败');
+            }
+          })
+        }else if(res.cancel) {
+          
+        }
+      }
+    })
   },
 
   /**
@@ -49,7 +88,9 @@ Page({
         console.log('success', res);
         if(res.data.status == 1){
           _this.setData({
-            dataObj: res.data.data
+            dataObj: res.data.data,
+            operationTime: timeTrans(res.data.data.operation_time),
+            whoHandle: res.data.data.who_handle
           })
         }
       },
@@ -60,7 +101,6 @@ Page({
     setInterval(function(){
       countDown(_this.data.dataObj.future_time*1000, _this);
     }, 300);
-
   },
 
   /**
