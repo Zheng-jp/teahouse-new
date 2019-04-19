d7eddfdb3b72d32017b3dd51457d78b2a4bdc658
zoume1 <595649828@qq.com>
2019-04-19 18:09:54 +0800

    pull

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ App({
       case "page":
         var t = n.indexOf("page/index"),
           o = n.indexOf("index?pageid"); -
-        1 == t || -1 != o ? wx.navigateTo({
+        1 == t || -1 != o ? wx.reLaunch({
           url: n
         }) : wx.reLaunch({
           url: n
@@ -196,7 +196,7 @@ App({
         break;
 
       case "web":
-        wx.navigateTo({
+        wx.reLaunch({
           url: n
         });
 
