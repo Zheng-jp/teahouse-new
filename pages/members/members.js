ad0e1ee9fb45aba1e89cf72196dcbc85eeae6979
Zheng-jp <970663468@qq.com>
2019-05-11 10:20:27 +0800

    pull001

--- pages/members/members.js
+++ pages/members/members.js
@@ -8,28 +8,40 @@ Page({
    */
   data: {
     tab: '0',
-    is: true, 
+    is: true,
     url: app.globalData.img_url,
-    level:[],
-    information:[ ],
-    menber_send: '0.00',
-    integral_send: '0.00'
-
+    level: [],
+    information: [],
+    member_send: '0.00',
+    integral_send: '0.00',
+    is_pay: false,
+    check: ''
   },
-    catchTouchMove:function(res){
+  catchTouchMove: function(res) {
     return false
   },
   /**
- * radio监听事件
- */
+   * radio监听事件
+   */
 
   // tab_slide: function (e) {//滑动切换tab 
   //   var that = this;
-   
+
   //   that.setData({ tab: e.detail.current });
   // },
- 
-  radioChange: function (e) {
+  read: function(e) {
+    var that = this;
+    if (e.detail.value == '') {
+      that.setData({
+        check: 0
+      })
+    } else {
+      that.setData({
+        check: 1
+      })
+    }
+  },
+  radioChange: function(e) {
     var that = this;
     // console.log(e)
     //  点击添加类
@@ -41,6 +53,12 @@ Page({
       })
     }
     var tab = that.data.tab;
+    console.log(that.data.level[tab])
+    console.log('-----------------------------')
+    that.setData({
+      member_send: that.data.level[tab].recharge_member_send,
+      integral_send: that.data.level[tab].recharge_integral_send
+    })
     for (var i = 0; i < that.data.level.length; i++) {
       var check = "level[" + i + "].check";
       that.setData({
@@ -55,124 +73,146 @@ Page({
     }
 
     var member_grade_id = that.data.information.member_grade_id;
-    
-    if (member_grade_id == that.data.level[tab].member_grade_id) {   
-        that.setData({
-          is: true,
-        })
-      }
-      else{
-        that.setData({
-          is: false,
-        })
-      }
-    
-    
+
+    if (member_grade_id == that.data.level[tab].member_grade_id) {
+      that.setData({
+        is: true,
+      })
+    } else {
+      that.setData({
+        is: false,
+      })
+    }
+
+
   },
-  stopTouchMove: function () {
+  stopTouchMove: function() {
     return false;
   },
-  bindViewTap: function () {
+  bindViewTap: function() {
     var that = this;
     wx.navigateTo({
       url: '../code/code?title=' + app.globalData.gmemberid,
-      success: function (res) {
-      },
-      fail: function () {
-      },
-      complete: function () {
-      }
+      success: function(res) {},
+      fail: function() {},
+      complete: function() {}
 
     })
   },
 
   //调起支付下单
-  pay: function (e) {
+  pay: function(e) {
     var that = this;
     var data = e.currentTarget;
-    console.log(that.data.level)
+    // console.log(that.data.level)
     var arr_data;
-    for(var i = 0; i < that.data.level.length; i++) {
-      if(that.data.level[i].check == true) {
-        arr_data = that.data.level[i];
+    if (that.data.check == '') {
+      wx.showToast({
+        title: '您是否阅读并同意《会员权益协议》',
+        icon: 'none',
+        duration: 3000
+      })
+    } else {
+      for (var i = 0; i < that.data.level.length; i++) {
+        if (that.data.level[i].check == true) {
+          arr_data = that.data.level[i];
+          if (that.data.level[i].member_grade_id <= that.data.information.member_grade_id) {
+            that.setData({
+              is_pay: false
+            })
+            wx.showToast({
+              title: '您当前等级为该档，您可选择其他等级补差额升级',
+              icon: 'none',
+              duration: 3000
+            })
+          } else {
+            that.setData({
+              is_pay: true
+            })
+          }
+        }
+      }
+      if (that.data.is_pay == true) {
+        //下单
+        wx.request({
+          url: app.globalData.tiltes + 'member_balance_recharge',
+          data: {
+            member_id: that.data.information.member_id, //账号id
+            money: arr_data.recharge_member_send, //金额
+            member_grade_id: arr_data.member_grade_id //选择等级
+          },
+          method: "POST",
+          success: function(res) {
+            // console.log(res)
+            //调起支付
+            wx.request({
+              url: app.globalData.tiltes + 'wx_recharge_pay',
+              data: {
+                member_id: that.data.information.member_id,
+                recharge_order_number: res.data.data
+              },
+              method: 'POST',
+              success: function(res) {
+                console.log(res)
+                var result = res;
+                if (result) {
+                  wx.requestPayment({
+                    timeStamp: String(result.data.timeStamp),
+                    nonceStr: result.data.nonceStr,
+                    package: result.data.package,
+                    signType: result.data.signType,
+                    paySign: result.data.paySign,
+                    'success': function(successret) {
+                      wx.showToast({
+                        title: '购买成功',
+                        icon: 'none',
+                        duration: 3000
+                      })
+                      wx.navigateBack({
+                        delta: 1
+                      })
+                    },
+                    'fail': function(e) {
+                      console.log(e)
+                    }
+                  })
+                }
+              },
+              fail: function(e) {
+
+              }
+            })
+          },
+          fail: function(e) {
+
+          },
+          complete: function() {}
+        })
       }
     }
-    console.log(arr_data)
-    //下单
-    wx.request({
-      url: app.globalData.tiltes + 'member_balance_recharge',
-      data: {
-        member_id: that.data.information.member_id,//账号id
-        money: arr_data.recharge_member_send,//金额
-        member_grade_id: arr_data.member_grade_id //选择等级
-      },
-      method: "POST",
-      success: function (res) {
-        // console.log(res)
-        //调起支付
-        wx.request({
-          url: app.globalData.tiltes + 'wx_recharge_pay',
-          data: {
-            member_id: that.data.information.member_id,
-            recharge_order_number: res.data.data
-          },
-          method: 'POST',
-          success: function (res) {
-            console.log(res)
-            var result = res;
-            if (result) {
-              wx.requestPayment({
-                timeStamp: String(result.data.timeStamp),
-                nonceStr: result.data.nonceStr,
-                package: result.data.package,
-                signType: result.data.signType,
-                paySign: result.data.paySign,
-                'success': function (successret) {
-                  console.log(successret)
-                  console.log('支付成功');
-                  // that.setData({
-                  //   apply: 1,
-                  // });
-                },
-                'fail': function (e) {
-                  console.log(e)
-                }
-              })
-            }
-          },
-          fail: function (e) {
 
-          }
-        })
-      },
-      fail: function (e) {
 
-      },
-      complete: function () {
-      }
-    })
   },
 
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-   
-    
+  onLoad: function(options) {
+
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    var gmemberid=app.globalData.gmemberid;
+  onShow: function() {
+    var gmemberid = app.globalData.gmemberid;
     var that = this;
     wx.request({
       url: app.globalData.tiltes + 'my_show_grade',
@@ -184,7 +224,7 @@ Page({
       //   "Content-Type": "json" // 默认值
 
       // },
-      success: function (res) {
+      success: function(res) {
         console.log(res.data.data.member_grade)
         that.setData({
           level: res.data.data.member_grade,
@@ -208,70 +248,71 @@ Page({
         var member_grade_id = that.data.information.member_grade_id;
         // console.log(member_grade_id);
         // console.log(that.data.level);
-        for (var index in that.data.level){
-        
-          if (member_grade_id == that.data.level[index].member_grade_id){
+        for (var index in that.data.level) {
+
+          if (member_grade_id == that.data.level[index].member_grade_id) {
             var check = "level[" + index + "].check";
-           
             that.setData({
+              member_send: that.data.level[index].recharge_member_send,
+              integral_send: that.data.level[index].recharge_integral_send,
               tab: that.data.level[index].tab,
-              [check]:true,
+              [check]: true,
             })
           }
         }
-        
-
-     
-       
-  
 
 
-      },
-      fail: function () {
+
+
+
+
 
       },
-      complete: function () {
+      fail: function() {
+
+      },
+      complete: function() {
         wx.hideLoading()
       }
 
     });
-     
-    
-  
+
+
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
