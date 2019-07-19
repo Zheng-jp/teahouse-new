08ad321c2812542bd20d16ebb9eb37af83e02edb
youdianku <571851168@qq.com>
2019-07-19 17:16:00 +0800

    push

--- storage/pages/stock_detail/stock_detail.js
+++ storage/pages/stock_detail/stock_detail.js
@@ -1,5 +1,33 @@
  // storage/pages/stock_detail/stock_detail.js
- const app = getApp();
+const app = getApp();
+var wxCharts = require('../../../component/wxcharts.js');
+var columnChart = null;
+
+var chartData = {
+  main: {
+    title: '总成交量',
+    data: [15, 20, 45, 37],
+    categories: ['2012', '2013', '2014', '2015']
+  },
+  sub: [{
+    title: '2012年度成交量',
+    data: [70, 40, 65, 100, 34, 18],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }, {
+    title: '2013年度成交量',
+    data: [55, 30, 45, 36, 56, 13],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }, {
+    title: '2014年度成交量',
+    data: [76, 45, 32, 74, 54, 35],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }, {
+    title: '2015年度成交量',
+    data: [76, 54, 23, 12, 45, 65],
+    categories: ['1', '2', '3', '4', '5', '6']
+  }]
+};
+
 Page({
 
   /**
@@ -43,8 +71,47 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function (e) {
+    var windowWidth = 375;
+    try {
+      var res = wx.getSystemInfoSync();
+      windowWidth = res.windowWidth;
+      console.log(windowWidth)
+    } catch (e) {
+      console.error('getSystemInfoSync failed!');
+    }
 
+    columnChart = new wxCharts({
+      canvasId: 'columnCanvas',
+      type: 'column',
+      animation: true,
+      categories: chartData.main.categories,
+      series: [{
+        name: '成交量',
+        data: chartData.main.data,
+        format: function (val, name) {
+          return val.toFixed(2) + '万';
+        }
+      }],
+      yAxis: {
+        format: function (val) {
+          return val + '万';
+        },
+        title: '元/件',
+        min: 0
+      },
+      xAxis: {
+        disableGrid: false,
+        type: 'calibration'
+      },
+      extra: {
+        column: {
+          width: 15
+        }
+      },
+      width: windowWidth,
+      height: 200,
+    });
   },
 
   /**
