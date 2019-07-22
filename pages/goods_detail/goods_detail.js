e2c9eaaa7a6f0d35e02fedc1eb095ab25688fff2
Zheng-jp <970663468@qq.com>
2019-07-22 15:17:11 +0800

    pull003

--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -657,7 +657,8 @@ Page({
     wx.request({
       url: app.globalData.tiltes + 'member_default_address_return',
       data: {
-        open_id: app.globalData.gmemberid,
+        member_id: app.globalData.member_id,
+        // open_id: app.globalData.gmemberid,
         address_id: ''
       },
       method: "post",
@@ -737,7 +738,8 @@ Page({
     wx.request({
       url: app.globalData.tiltes + 'member_default_address_return',
       data: {
-        open_id: app.globalData.gmemberid,
+        member_id: app.globalData.member_id,
+        // open_id: app.globalData.gmemberid,
         address_id: ''
       },
       method: "post",
