21839515aab43f7edb068f76298728311a7e7503
youdianku <571851168@qq.com>
2019-07-23 17:58:32 +0800

    push

--- storage/pages/out_of_warehouse/out_of_warehouse.js
+++ storage/pages/out_of_warehouse/out_of_warehouse.js
@@ -76,7 +76,7 @@ Page({
   // 获取默认地址
   getDefaultAddress: function(){
     var _this = this;
-    var params = {open_id: app.globalData.gmemberid}
+    var params = { member_id: app.globalData.member_id }
     _this.myRequest('member_default_address_return', params, function(res){
       console.log('获取默认地址：', res);
       if(res.data.status == 1){
